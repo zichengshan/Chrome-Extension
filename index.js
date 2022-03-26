f3f91c0d0ac6c2a01c6ec20904b8b76fea26cc46
Zicheng.S <zichengshan@gmail.com>
2022-03-25 21:04:39 -0700

    innerHTML to render <li>

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
