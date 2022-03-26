cae8225d88a5b4eb3aa156580ad863e27ece4d8b
Zicheng.S <zichengshan@gmail.com>
2022-03-25 22:29:51 -0700

    style the list

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function renderLeads() {
     for (let i = 0; i < myLeads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
+                <a target="_blank" href="${myLeads[i]}">
                     ${myLeads[i]}
                 </a>
             </li>
