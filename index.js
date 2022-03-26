0b5be9503521c5e1fad4edab32fc610c43d3b15d
Zicheng.S <zichengshan@gmail.com>
2022-03-25 20:58:51 -0700

    render <li> element

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 let myLeads = []
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
+
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
