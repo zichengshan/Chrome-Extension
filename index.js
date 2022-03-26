70cebc7af68a28463d9f99c86336716e809ed350
Zicheng.S <zichengshan@gmail.com>
2022-03-25 23:09:33 -0700

    get leads from localStorage

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
