ec50f318b1a1693b4bf7789eaa81a4b42918cd1c
Zicheng.S <zichengshan@gmail.com>
2022-03-25 23:03:32 -0700

    save leads to the localStorage

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const ulEl = document.getElementById("ul-el")
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
     renderLeads()
+    console.log( localStorage.getItem("myLeads") )
 })
 
 function renderLeads() {
